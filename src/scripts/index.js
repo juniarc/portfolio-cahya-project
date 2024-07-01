27467745ed1b230cdf47b19466b12fb2309d9e4c
cahya <juniarcahya@gmail.com>
2024-07-01 21:07:45 +0700

    add resposive style

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,19 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.scss'
+
+const burgerBtn = document.querySelector('#burgerBtn');
+const navMenu = document.querySelector('#navMenu');
+const closeBtn = document.querySelector('#closeBtn');
+const navItem = document.querySelector('#navItem');
+
+burgerBtn.addEventListener('click', () => {
+    navMenu.classList.toggle('open');
+});
+
+closeBtn.addEventListener('click', () => {
+    navMenu.classList.remove('open');
+});
+
+navItem.addEventListener('click', () => {
+    navMenu.classList.remove('open');
+})
